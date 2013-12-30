711f176bca88bab577fd9151f4755b7f11d78321
Seth Fitzsimmons <seth@mojodna.net>
2013-12-29 19:37:42 -0800

    Initial version

---
+++ README.md
@@ -0,0 +1,49 @@
+# log-nexus
+
+I am a rebroadcasting syslog server. I accept syslog messages and forward them
+over HTTP and WebSocket connections.
+
+## Why?
+
+[Heroku](http://heroku.com/) and [Fastly](http://fastly.com/) support remote
+syslog drains. [Papertrail](http://papertrailapp.com/) is a good choice for
+bridging logs to client processes, but it comes with additional functionality
+(searching, archiving, etc.) and associated costs. This is simpler and comes
+with none of the frills.
+
+You can also think of this as an alternative to `heroku logs -t`.
+
+## Installation
+
+```bash
+npm install -g log-nexus
+```
+
+## Usage
+
+```bash
+log-nexus -s <syslog port> -p <http/ws port>
+```
+
+To feed data in locally:
+
+```bash
+tail -f /var/log/syslog | nc localhost 8515
+```
+
+Meanwhile:
+
+```bash
+http://localhost:8080/
+```
+
+or, using `wscat` (available as part of [`ws`](http://einaros.github.io/ws/)):
+
+```bash
+wscat --connect ws://localhost:8080
+```
+
+## Limitations
+
+All streams are managed in-process, so this will only work when a single
+instance of `log-nexus` is running.
---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+"use strict";
+
+var http = require("http");
+
+var InfinitePassThrough = require("./lib/infinite-passthrough"),
+    SkippingStream = require("./lib/skipping-stream"),
+    makeApp = require("./lib/app"),
+    makeSyslog = require("./lib/syslog"),
+    makeWS = require("./lib/websocket");
+
+var opts = require("commander")
+  .version(require("./package.json").version)
+  .usage("[options]")
+  // .option("-r, --remote <url>", "Remote source")
+  .option("-s, --syslog <port>", "Syslog port (defaults to 8514)", parseInt, 8514)
+  // .option("-S, --no-syslog", "Don't start syslog server") // only makes
+  // sense with remote sources
+  .option("-p, --port <port>", "HTTP/WS port (defaults to 8080)", parseInt, 8080)
+  .parse(process.argv);
+
+// var nexus = new InfinitePassThrough().pipe(new SkippingStream()),
+var nexus = new InfinitePassThrough(),
+    app = makeApp(nexus),
+    server = http.createServer(app);
+
+// prime the nexus
+nexus.write("\n");
+
+makeWS(server, nexus);
+
+server.listen(opts.port, function() {
+  console.log("Listening at {http,ws}://%s:%d", this.address().address, this.address().port);
+});
+
+makeSyslog(nexus).listen(opts.syslog, function() {
+  console.log("Listening at tcp://%s:%d", this.address().address, this.address().port);
+});
+
